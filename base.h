c0fe833f40655e8db009054a958f775e91988c41
AlexanderBateson <falex7835@gmail.com>
2020-01-18 01:15:30 +0300

    Create base.h

---
+++ base.h
@@ -0,0 +1,19 @@
+#ifndef HBASE
+#define HBASE
+
+typedef unsigned long long	u64;
+typedef unsgined long		u32;
+typedef unsigned short		u16;
+typedef unsigned char		u8;
+
+typedef u64					quant64;
+typedef u32					quant32;
+typedef u16					quant16;
+typedef u8					quant8;
+
+typedef u64					index64;
+typedef u32					index32;
+typedef u16					index16;
+typedef u8					index8;
+
+#endif
